0610e708bbc7117121907500f22b690b488f96ca
Denis Komelkov <den.komelkov@cimpleo.com>
2016-12-07 13:34:28 +0600

    slider for demensions.

---
+++ sites/all/modules/modelplatform/modules/mp_forms/js/mp_forms.js
@@ -0,0 +1,29 @@
+/**
+ * Created by cimpleo on 07.12.16.
+ */
+
+(function ($) {
+
+  Drupal.behaviors.mp_forms = {
+    attach: function (context, settings) {
+      $('body').once(function() {
+        var _sliders = settings.mp_forms.sliders;
+        $.each(_sliders, function (i, slider) {
+          var _value = $(slider.selector + " input");
+          var wrapper = $(slider.selector);
+          wrapper.slider({
+            range: false,
+            min: parseInt(slider.min),
+            max: parseInt(slider.max),
+            value: _value.val(),
+            slide: function (event, ui) {
+              _value.val(ui.value);
+            }
+          });
+          wrapper.before(slider.label);
+        });
+      });
+    }
+  };
+
+})(jQuery);
