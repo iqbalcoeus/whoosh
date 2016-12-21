3c63fd8643f9e9eb4e33907c15305d9a057fb1fe
Denis Komelkov <den.komelkov@cimpleo.com>
2016-12-21 09:46:51 +0600

    MP-26: Registration for customer

--- sites/all/modules/modelplatform/modules/mp_forms/js/mp_forms.js
+++ sites/all/modules/modelplatform/modules/mp_forms/js/mp_forms.js
@@ -6,23 +6,40 @@
 
   Drupal.behaviors.mp_forms = {
     attach: function (context, settings) {
+      // Sliders.
       $('body').once(function() {
-        var _sliders = settings.mp_forms.sliders;
-        $.each(_sliders, function (i, slider) {
-          var _value = $(slider.selector + " input");
-          var wrapper = $(slider.selector);
-          wrapper.slider({
-            range: false,
-            min: parseInt(slider.min),
-            max: parseInt(slider.max),
-            value: _value.val(),
-            slide: function (event, ui) {
-              _value.val(ui.value);
-            }
+        if (settings.mp_forms.sliders !== undefined) {
+          var _sliders = settings.mp_forms.sliders;
+          $.each(_sliders, function (i, slider) {
+            var _value = $(slider.selector + " input");
+            var wrapper = $(slider.selector);
+            wrapper.slider({
+              range: false,
+              min: parseInt(slider.min),
+              max: parseInt(slider.max),
+              value: _value.val(),
+              slide: function (event, ui) {
+                _value.val(ui.value);
+              }
+            });
           });
-        });
+        }
       });
     }
   };
+
+  $(document).ready(function() {
+    $('#edit-user-roles input').click(function() {
+      var socialBlock = $('.block-social-login-widget .social_login')
+      if ($(this).val() == Drupal.settings.mp_forms.model) {
+        socialBlock.addClass('model');
+        socialBlock.removeClass('customer');
+      }
+      else {
+        socialBlock.addClass('customer');
+        socialBlock.removeClass('model');
+      }
+    });
+  });
 
 })(jQuery);
