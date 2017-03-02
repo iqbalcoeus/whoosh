350a79e66992084ada411438e5f3394d59ca653c
Denis Komelkov <den.komelkov@cimpleo.com>
2017-03-02 14:28:15 +0600

    youtube paralax.

--- sites/all/modules/modelplatform/js/modelplatform.js
+++ sites/all/modules/modelplatform/js/modelplatform.js
@@ -4,7 +4,9 @@
     var w = Drupal.settings.modelplatform.video_width;
     var h = Drupal.settings.modelplatform.video_height;
     var el = $('#front_page_video');
+    var elBlock = $('.front-page-video-block');
     el.height(el.width() * h / w);
+    elBlock.height(el.width() * h / w);
   };
 
   $(document).ready(resizeAction);
