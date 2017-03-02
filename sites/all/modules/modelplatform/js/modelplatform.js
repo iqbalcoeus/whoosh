f52baf91c89c900abb501fb6a8cd44a1bd6e8e92
Denis Komelkov <den.komelkov@cimpleo.com>
2017-03-02 17:11:48 +0600

    Video block. play button.

--- sites/all/modules/modelplatform/js/modelplatform.js
+++ sites/all/modules/modelplatform/js/modelplatform.js
@@ -1,8 +1,8 @@
 (function ($) {
 
   function resizeAction() {
-    var w = Drupal.settings.modelplatform.video_width;
-    var h = Drupal.settings.modelplatform.video_height;
+    var w = Drupal.settings.modelplatform.videoWidth;
+    var h = Drupal.settings.modelplatform.videoHeight;
     var el = $('#front_page_video');
     var elBlock = $('.front-page-video-block');
     var wh = $(window).height();
@@ -20,7 +20,64 @@
     }
   };
 
-  $(document).ready(resizeAction);
+  $(document).ready(function(){
+    resizeAction();
+    var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/player_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    Drupal.youtube_player = null || Drupal.youtube_player;
+  });
   $(window).resize(resizeAction);
 
 })(jQuery);
+
+function onPlayerStateChange(newState) {
+  if (Drupal.youtubeFlag) {
+    Drupal.youtubeFlag = false;
+    Drupal.youtube_player.pauseVideo();
+  }
+  if (newState.data == 1) {
+    jQuery('.front-page-video-block').addClass('playing');
+  }
+  else {
+    jQuery('.front-page-video-block').removeClass('playing');
+  }
+}
+
+function onYouTubePlayerAPIReady() {
+  Drupal.youtube_player = new YT.Player('ytplayer', {
+      width: '100%',
+      height: '100%',
+      videoId: Drupal.settings.modelplatform.videoID,
+      playerVars: {
+        controls: 0,
+        disablekb: 0,
+        showinfo: 0,
+        autoplay: 1,
+        loop: 1,
+      },
+      events: {
+        'onStateChange': onPlayerStateChange
+      },
+      suggestedQuality: 'hq720',
+    });
+
+  Drupal.youtubeFlag = true;
+
+  var _play = '<div id="play-button"></div>';
+  var _pause = '<div id="pause-button"></div>';
+
+  jQuery('.front-page-video-block').append(_play).append(_pause);
+
+  var playButton = document.getElementById("play-button");
+  playButton.addEventListener("click", function() {
+    Drupal.youtube_player.playVideo();
+  });
+
+  var pauseButton = document.getElementById("pause-button");
+  pauseButton.addEventListener("click", function() {
+    Drupal.youtube_player.pauseVideo();
+  });
+}
