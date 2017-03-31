7b436bbd2fc8b6bbc53c2acd8c590f1cd4895c73
Denis Komelkov <den.komelkov@cimpleo.com>
2017-03-31 16:45:39 +0600

    massonry to model search file

--- sites/all/modules/modelplatform/js/model_search.js
+++ sites/all/modules/modelplatform/js/model_search.js
@@ -26,9 +26,74 @@
     }
   };
 
+  function prepareModelSearch() {
+    // Filter button action.
+    $('#models_search_filter').click(function(){
+      var $filters = $('.view-models .view-filters');
+      if ($filters.hasClass('active')) {
+        $(this).removeClass('active');
+        $filters.removeClass('active');
+      }
+      else {
+        $(this).addClass('active');
+        $filters.addClass('active');
+      }
+    });
+
+    // Search.
+    var $searchField = $('#models_search_by_name');
+    $searchField.val($('.form-control[name="field_full_name_value"]').val());
+    $searchField.keypress(function (e) {
+      if (e.which == 13) {
+        $('.form-control[name="field_full_name_value"]').val($(this).val());
+        $('.view-models form').submit();
+      }
+    });
+
+    // collapsed.
+    $('.view-filters label').click(function(){
+      var $parent = $(this).parent();
+
+      if ($parent.hasClass('active')) {
+        $parent.removeClass('active')
+      }
+      else {
+        $parent.addClass('active');
+      }
+    });
+
+    //
+    $('.form-item-field-daily-rate-value-1 input').attr('placeholder', '');
+    $('.form-item-field-lingerie-daily-rate-value input').attr('placeholder', '');
+    if ($('#edit-field-lingerie-value-1:checked').length) {
+      $('.form-item-field-daily-rate-value-1').hide();
+      $('.form-item-field-lingerie-daily-rate-value').show();
+    }
+    $('.form-item-field-lingerie-value input').change(function(){
+      if (this.checked) {
+        $('.form-item-field-daily-rate-value-1').hide();
+        $('.form-item-field-lingerie-daily-rate-value').show();
+      }
+      else {
+        $('.form-item-field-lingerie-daily-rate-value').hide();
+        $('.form-item-field-daily-rate-value-1').show();
+      }
+    });
+
+    var $grid = $('.view-models .view-content').masonry({
+      itemSelector: '.views-row',
+      //columnWidth: 283,
+      fitWidth: true
+    });
+    // layout Masonry after each image loads
+    $grid.imagesLoaded().progress( function() {
+      $grid.masonry('layout');
+    });
+  };
+
   $(document).ready(function(){
+    prepareModelSearch();
     heightSlider();
   });
-
 
 })(jQuery);
