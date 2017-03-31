4b46a349fffca554e6e81b938402e1aac53a4323
Denis Komelkov <den.komelkov@cimpleo.com>
2017-03-31 15:40:09 +0600

    Height div

--- sites/all/modules/modelplatform/js/model_search.js
+++ sites/all/modules/modelplatform/js/model_search.js
@@ -5,9 +5,12 @@
       var heightSettings = Drupal.settings.better_exposed_filters.slider_options.field_height_value;
       var heightValue = $('.form-item-field-height-value');
 
+      $('.form-item-field-height-value').append('<div id="height_slider_value"></div>');
+
       if (heightValue.length) {
         heightValue.once('add-slider', function () {
           $("#edit-field-height-value").val(parseInt(heightSettings.min));
+          $("#height_slider_value").html((parseInt(heightSettings.min) / 100) + ' M');
           heightValue.prepend('<div id="romira-fixed-price-slider"></div>');
           $("#romira-fixed-price-slider").slider({
             min: parseInt(heightSettings.min),
@@ -15,6 +18,7 @@
             step: heightSettings.step,
             slide: function(event, ui) {
               $("#edit-field-height-value").val(ui.value);
+              $("#height_slider_value").html((ui.value / 100) + ' M');
             }
           });
         });
