9d7fd10e361cf98549fc9c566ea3417f8d1bcfdd
Denis Komelkov <den.komelkov@cimpleo.com>
2017-04-21 16:25:32 +0600

    Dimensions

---
+++ sites/all/modules/modelplatform/js/model_params.js
@@ -0,0 +1,26 @@
+/**
+ * Created by cimpleo on 21.04.17.
+ */
+
+(function ($) {
+
+  Drupal.behaviors.modelplatform_params = {
+    attach: function (context, settings) {
+      var selectors = Drupal.settings.modelplatform_params.selectors;
+      $.each(selectors, function(i, sel) {
+        var item = $(sel);
+        var divideNum = 1;
+        var suffix = '';
+        if (Drupal.settings.modelplatform_theme.lang === 'en') {
+          divideNum = 2.54;
+          suffix = '"';
+        }
+        var value = parseInt(item.html());
+        value = Math.round(value / divideNum);
+        item.html(value + suffix);
+      });
+    }
+  };
+
+})(jQuery);
+
