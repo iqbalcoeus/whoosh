c694a33196f591baca7ff87d84171876c1124ec7
Denis Komelkov <den.komelkov@cimpleo.com>
2017-04-21 16:30:49 +0600

    theme check

--- sites/all/modules/modelplatform/js/model_params.js
+++ sites/all/modules/modelplatform/js/model_params.js
@@ -6,19 +6,21 @@
 
   Drupal.behaviors.modelplatform_params = {
     attach: function (context, settings) {
-      var selectors = Drupal.settings.modelplatform_params.selectors;
-      $.each(selectors, function(i, sel) {
-        var item = $(sel);
-        var divideNum = 1;
-        var suffix = '';
-        if (Drupal.settings.modelplatform_theme.lang === 'en') {
-          divideNum = 2.54;
-          suffix = '';
-        }
-        var value = parseInt(item.html());
-        value = Math.round(value / divideNum);
-        item.html(value + suffix);
-      });
+      if (Drupal.settings.modelplatform_theme) {
+        var selectors = Drupal.settings.modelplatform_params.selectors;
+        $.each(selectors, function (i, sel) {
+          var item = $(sel);
+          var divideNum = 1;
+          var suffix = '';
+          if (Drupal.settings.modelplatform_theme.lang === 'en') {
+            divideNum = 2.54;
+            suffix = '';
+          }
+          var value = parseInt(item.html());
+          value = Math.round(value / divideNum);
+          item.html(value + suffix);
+        });
+      }
     }
   };
 
