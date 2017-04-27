bd029fca7c729571f4fb8169b7f543391cfdca99
Denis Komelkov <den.komelkov@cimpleo.com>
2017-04-27 13:28:13 +0600

    js once added for messuirments

--- sites/all/modules/modelplatform/js/model_params.js
+++ sites/all/modules/modelplatform/js/model_params.js
@@ -7,18 +7,20 @@
   Drupal.behaviors.modelplatform_params = {
     attach: function (context, settings) {
       if (Drupal.settings.modelplatform_theme) {
-        var selectors = Drupal.settings.modelplatform_params.selectors;
-        $.each(selectors, function (i, sel) {
-          var item = $(sel);
-          var divideNum = 1;
-          var suffix = '';
-          if (Drupal.settings.modelplatform_theme.lang === 'en') {
-            divideNum = 2.54;
-            suffix = '';
-          }
-          var value = parseInt(item.html());
-          value = Math.round(value / divideNum);
-          item.html(value + suffix);
+        $('body').once(function() {
+          var selectors = Drupal.settings.modelplatform_params.selectors;
+          $.each(selectors, function (i, sel) {
+            var item = $(sel);
+            var divideNum = 1;
+            var suffix = '';
+            if (Drupal.settings.modelplatform_theme.lang === 'en') {
+              divideNum = 2.54;
+              suffix = '';
+            }
+            var value = parseInt(item.html());
+            value = Math.round(value / divideNum);
+            item.html(value + suffix);
+          });
         });
       }
     }
