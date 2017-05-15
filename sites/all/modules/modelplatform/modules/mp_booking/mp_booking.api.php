fc2a9f5029bab50968232d858dc97a31e04dd037
Denis Komelkov <den.komelkov@cimpleo.com>
2017-05-15 17:11:52 +0600

    Booking functionality. And new hooks.

---
+++ sites/all/modules/modelplatform/modules/mp_booking/mp_booking.api.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @file
+ * Model Platform Booking API.
+ */
+
+/**
+ * After model was optioned.
+ */
+function hook_mpb_option_model($event) {
+
+}
+
+/**
+ * After model was optioned and confirmed.
+ */
+function hook_mpb_option_model_accept($event) {
+
+}
+
+/**
+ * After model was optioned and confirmed.
+ */
+function hook_mpb_option_model_decline($event) {
+
+}
