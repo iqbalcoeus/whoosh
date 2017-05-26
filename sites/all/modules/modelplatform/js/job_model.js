e79056f24729d528734c269d3f065969517008c5
Denis Komelkov <den.komelkov@cimpleo.com>
2017-05-26 15:21:19 +0600

    massonry for job models

---
+++ sites/all/modules/modelplatform/js/job_model.js
@@ -0,0 +1,22 @@
+/**
+ * Created by cimpleo on 26.05.17.
+ */
+
+(function ($) {
+
+  Drupal.behaviors.modelplatform = {
+    attach: function (context, settings) {
+      // Masonry part.
+      var $grid = $('.view-job-models .view-content').masonry({
+        itemSelector: '.views-row',
+        fitWidth: true
+      });
+
+      // layout Masonry after each image loads
+      $grid.imagesLoaded().progress( function() {
+        $grid.masonry('layout');
+      });
+    }
+  };
+
+})(jQuery);
