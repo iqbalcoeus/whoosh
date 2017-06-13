fee345fb3252d8221a499390f9b0ddac787a7cf5
Denis Komelkov <den.komelkov@cimpleo.com>
2017-06-13 12:32:09 +0600

    Lingerie fix.

--- sites/all/modules/modelplatform/modules/mp_forms/js/additional_info.js
+++ sites/all/modules/modelplatform/modules/mp_forms/js/additional_info.js
@@ -4,18 +4,20 @@
 
 (function ($) {
 
+  function checkLingerie() {
+    if ($('input[name="profile_model_additional_info[field_lingerie][und]"').is(":checked")) {
+      $('.field-name-field-lingerie-daily-rate').css('display', 'block');
+      $('.field-name-field-lingerie-daily-rate input').attr('required', 'required');
+    }
+    else {
+      $('.field-name-field-lingerie-daily-rate').css('display', 'none');
+      $('.field-name-field-lingerie-daily-rate input').attr('required', '');
+    }
+  }
   Drupal.behaviors.mp_forms = {
     attach: function (context, settings) {
-      $('input[name="profile_model_additional_info[field_lingerie][und]"').change(function() {
-        if (this.checked) {
-          $('.field-name-field-lingerie-daily-rate').css('display', 'block');
-          $('.field-name-field-lingerie-daily-rate input').attr('required', 'required');
-        }
-        else {
-          $('.field-name-field-lingerie-daily-rate').css('display', 'none');
-          $('.field-name-field-lingerie-daily-rate input').attr('required', '');
-        }
-      });
+      checkLingerie();
+      $('input[name="profile_model_additional_info[field_lingerie][und]"').change(checkLingerie);
     }
   };
 
