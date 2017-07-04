3e62acf1d3e7d9da119459ccad3dec409fe9b365
Denis Komelkov <den.komelkov@cimpleo.com>
2017-07-04 16:06:16 +0600

    Job rating new feature.

--- sites/all/modules/modelplatform/modules/mp_rating/js/mp_rating.js
+++ sites/all/modules/modelplatform/modules/mp_rating/js/mp_rating.js
@@ -8,9 +8,14 @@
     attach: function (context, settings) {
       $('.past-job-rating h5').css('margin-top', ($('.past-job-rating').height() * 0.3) + 'px');
       $('.past-job-rating').click(function(event){
-        if ($(event.target).hasClass('past-job-rating') ||
-          $(event.target).parent().hasClass('star')) {
-          $('.past-job-rating').hide();
+        if ($(event.target).hasClass('past-job-rating')) {
+          $('.past-job-rating').remove();
+        }
+        if ($(event.target).parent().hasClass('star')) {
+          $(event.target).parents('.rating-row').remove();
+          if ($('.past-job-rating .rating-row').length == 0) {
+            $('.past-job-rating').remove();
+          }
         }
       });
     }
