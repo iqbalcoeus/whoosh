362548a9a6ea8c34c08782cc4d6a778eb8bc8a98
Denis Komelkov <den.komelkov@cimpleo.com>
2017-07-31 16:10:14 +0600

    masonry disabled for jobs

--- sites/all/modules/modelplatform/js/job_model.js
+++ sites/all/modules/modelplatform/js/job_model.js
@@ -6,16 +6,16 @@
 
   Drupal.behaviors.modelplatform = {
     attach: function (context, settings) {
-      // Masonry part.
-      var $grid = $('.view-job-models .view-content').masonry({
-        itemSelector: '.views-row',
-        fitWidth: true
-      });
-
-      // layout Masonry after each image loads
-      $grid.imagesLoaded().progress( function() {
-        $grid.masonry('layout');
-      });
+      // // Masonry part.
+      // var $grid = $('.view-job-models .view-content').masonry({
+      //   itemSelector: '.views-row',
+      //   fitWidth: true
+      // });
+      //
+      // // layout Masonry after each image loads
+      // $grid.imagesLoaded().progress( function() {
+      //   $grid.masonry('layout');
+      // });
     }
   };
 
