640c61d5df180f55d0452c6f8bd5844c6fe89fdd
kRaath <khadim.raath@gmail.com>
2018-05-28 02:45:29 +0500

    download model images feature, Design fixes (MP-172, MP-174)

--- sites/all/modules/modelplatform/js/model_params.js
+++ sites/all/modules/modelplatform/js/model_params.js
@@ -28,3 +28,44 @@
 
 })(jQuery);
 
+// Download images feature
+jQuery( document ).ready(function() {
+
+  var imgUrls = jQuery('#quicktabs-container-model_gallery').find('img').map(function() { return this.src; }).get();
+
+  if(imgUrls.length>0){
+    jQuery("#quicktabs-model_gallery").append('<div class="form-actions form-wrapper"><input class="btn-medium btn btn-mod btn-round form-submit" type="button" id="download-images" name="op" value="Download"></div>');
+
+    jQuery("#download-images").click(function() {
+
+      var zip = new JSZip();
+      var count = 0;
+      var zipFilename = "model_images.zip";
+
+      var imgTitle = 1;
+      imgUrls.forEach(function(url){
+
+        var filename = "img_"+imgTitle+".png";
+
+        imgTitle = imgTitle+1;
+
+        // loading a file and add it in a zip file
+        JSZipUtils.getBinaryContent(url, function (err, data) {
+         if(err) {
+              throw err; // or handle the error
+              console.log(err);
+            }
+
+            zip.file(filename, data, {binary:true});
+            count++;
+
+            if (count == imgUrls.length) {
+             var zipFile = zip.generate({type: "blob"});
+             saveAs(zipFile, zipFilename);
+           }
+         });
+      });
+
+    });
+  }
+});
