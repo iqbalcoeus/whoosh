6909fcb73278aff8baf0b3040ccd529d43f818fa
iqbalcoeus <muhammad.iqbal@coeus-solutions.de>
2018-07-04 19:05:58 +0500

    fixing general issues

--- sites/all/themes/rhythm/template.php
+++ sites/all/themes/rhythm/template.php
@@ -705,52 +705,52 @@ function rhythm_pager($variables) {
   }
 }
 
-function rhythm_pager_link($variables) {
-  $text = $variables['text'];
-  $page_new = $variables['page_new'];
-  $element = $variables['element'];
-  $parameters = $variables['parameters'];
-  $attributes = $variables['attributes'];
+// function rhythm_pager_link($variables) {
+//   // $text = $variables['text'];
+//   // $page_new = $variables['page_new'];
+//   // $element = $variables['element'];
+//   // $parameters = $variables['parameters'];
+//   // $attributes = $variables['attributes'];
 
-  $page = isset($_GET['page']) ? $_GET['page'] : '';
-  if ($new_page = implode(',', pager_load_array($page_new[$element], $element, explode(',', $page)))) {
-    $parameters['page'] = $new_page;
-  }
+//   // $page = isset($_GET['page']) ? $_GET['page'] : '';
+//   // if ($new_page = implode(',', pager_load_array($page_new[$element], $element, explode(',', $page)))) {
+//   //   $parameters['page'] = $new_page;
+//   // }
 
-  $query = array();
-  if (count($parameters)) {
-    $query = drupal_get_query_parameters($parameters, array());
-  }
-  if ($query_pager = pager_get_query_parameters()) {
-    $query = array_merge($query, $query_pager);
-  }
+//   // $query = array();
+//   // if (count($parameters)) {
+//   //   $query = drupal_get_query_parameters($parameters, array());
+//   // }
+//   // if ($query_pager = pager_get_query_parameters()) {
+//   //   $query = array_merge($query, $query_pager);
+//   // }
 
-  // Set each pager link title
-  if (!isset($attributes['title'])) {
-    static $titles = NULL;
-    if (!isset($titles)) {
-      $titles = array(
-        t('«') => t('Go to first page'),
-        t('‹') => t('Go to previous page'),
-        t('›') => t('Go to next page'),
-        t('»') => t('Go to last page'),
-      );
-    }
-    if (isset($titles[$text])) {
-      $attributes['title'] = $titles[$text];
-    }
-    elseif (is_numeric($text)) {
-      $attributes['title'] = t('Go to page @number', array('@number' => $text));
-    }
-  }
-  $replace_titles = array(
-    t('« first') => '<i class="fa fa-angle-double-left"></i>',
-    t('‹ previous') => '<i class="fa fa-angle-left"></i>',
-    t('next ›') => '<i class="fa fa-angle-right"></i>',
-    t('last »') => '<i class="fa fa-angle-double-right"></i>',
-  );
-  $text = isset($replace_titles[$text]) ? $replace_titles[$text] : check_plain($text);
+//   // // Set each pager link title
+//   // if (!isset($attributes['title'])) {
+//   //   static $titles = NULL;
+//   //   if (!isset($titles)) {
+//   //     $titles = array(
+//   //       t('«') => t('Go to first page'),
+//   //       t('‹') => t('Go to previous page'),
+//   //       t('›') => t('Go to next page'),
+//   //       t('»') => t('Go to last page'),
+//   //     );
+//   //   }
+//   //   if (isset($titles[$text])) {
+//   //     $attributes['title'] = $titles[$text];
+//   //   }
+//   //   elseif (is_numeric($text)) {
+//   //     $attributes['title'] = t('Go to page @number', array('@number' => $text));
+//   //   }
+//   // }
+//   // $replace_titles = array(
+//   //   t('« first') => '<i class="fa fa-angle-double-left"></i>',
+//   //   t('‹ previous') => '<i class="fa fa-angle-left"></i>',
+//   //   t('next ›') => '<i class="fa fa-angle-right"></i>',
+//   //   t('last »') => '<i class="fa fa-angle-double-right"></i>',
+//   // );
+//   // $text = isset($replace_titles[$text]) ? $replace_titles[$text] : check_plain($text);
 
-  $attributes['href'] = url($_GET['q'], array('query' => $query));
-  return '<a' . drupal_attributes($attributes) . '>' . $text . '</a>';
-}
+//   // $attributes['href'] = url($_GET['q'], array('query' => $query));
+//   // return '<a' . drupal_attributes($attributes) . '>' . $text . '</a>';
+// }
